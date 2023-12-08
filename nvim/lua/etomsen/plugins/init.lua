bcc7f0b60bcc89d7282ad9c8ba4aa4c3f67aee0f
Evgeny Tomsen <e.tomsen@solhea.com>
2023-12-08 23:27:46 +0300

    convery nvim to lazy.nvim package manager, fix tree-sitter bug with tmux wrong arch

--- nvim/lua/etomsen/plugins/init.lua
+++ nvim/lua/etomsen/plugins/init.lua
@@ -1,44 +1,51 @@
-local Plug = require 'usermod.vimplug'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-Plug.begin('~/.config/nvim/plugged')
-Plug 'junegunn/fzf'
-Plug 'junegunn/fzf.vim'
-Plug 'tpope/vim-surround'
-Plug 'tpope/vim-commentary'
-Plug 'tpope/vim-repeat'
-Plug 'airblade/vim-gitgutter' -- git changes shown inline, integrated with airline
-Plug 'vim-airline/vim-airline' -- a bottom bar with info
-Plug 'nvim-lua/plenary.nvim' -- utills
-Plug 'ap/vim-buftabline' -- a top bar with opened buffers
-Plug 'ThePrimeagen/harpoon' -- recent files
-Plug('folke/todo-comments.nvim') -- grep on comments
-Plug('nvim-telescope/telescope.nvim', {
-  tag = '0.1.0'
-})
-Plug('nvim-telescope/telescope-fzf-native.nvim', { run = 'make' })
-Plug 'morhetz/gruvbox' -- theme
-Plug 'stevearc/oil.nvim' -- file management
-
--- LSP and syntax
-Plug 'neovim/nvim-lspconfig'
-Plug 'MunifTanjim/prettier.nvim' -- prettier for LSP
-Plug 'folke/neodev.nvim' -- nvim dev helpers for LSP
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'ray-x/lsp_signature.nvim'
-Plug ('nvim-treesitter/nvim-treesitter') -- better syntax highlighting
-
--- GIT
-Plug 'APZelos/blamer.nvim' -- git blame
-Plug 'tpope/vim-fugitive'
-
--- TMUX
-Plug 'preservim/vimux' -- tmux
-Plug 'joeveiga/ng.nvim' -- to run tmux from vim
-Plug 'christoomey/vim-tmux-navigator' -- to navigate vim splits together with tmux
-Plug 'tyewang/vimux-jest-test' -- tmux jest runner
-
-Plug.ends()
+local plugins = {
+  'junegunn/fzf',
+  'junegunn/fzf.vim',
+  'tpope/vim-surround',
+  'tpope/vim-commentary',
+  'tpope/vim-repeat',
+  'airblade/vim-gitgutter', -- git changes shown inline, integrated with airline
+  'vim-airline/vim-airline', -- a bottom bar with info
+  'nvim-lua/plenary.nvim', -- utills
+  'ap/vim-buftabline', -- a top bar with opened buffers
+  'ThePrimeagen/harpoon', -- recent files
+  'folke/todo-comments.nvim',  -- grep on comments
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  'princejoogie/dir-telescope.nvim',
+  'morhetz/gruvbox', -- theme
+  'stevearc/oil.nvim', -- file management
+  -- LSP and syntax
+  'neovim/nvim-lspconfig',
+  'MunifTanjim/prettier.nvim', -- prettier for LSP
+  'folke/neodev.nvim', -- nvim dev helpers for LSP
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'ray-x/lsp_signature.nvim',
+  'nvim-treesitter/nvim-treesitter', -- better syntax highlighting
+  -- GIT
+  'APZelos/blamer.nvim', -- git blame
+  'tpope/vim-fugitive',
+  -- TMUX
+  'preservim/vimux', -- tmux
+  'joeveiga/ng.nvim', -- to run tmux from vim
+  'christoomey/vim-tmux-navigator', -- to navigate vim splits together with tmux
+  'tyewang/vimux-jest-test' -- tmux jest runner
+}
+require("lazy").setup(plugins)
--- nvim/lua/etomsen/telescope/init.lua
+++ nvim/lua/etomsen/telescope/init.lua
@@ -4,6 +4,7 @@ function M.setup()
   local actions = require "telescope.actions"
   local telescope = require "telescope"
   telescope.load_extension "fzf"
+  telescope.load_extension "dir"
 end
 
 return M
